fdeecdd377e1b4c738650ce16bffdef7c4d5373d
EJLarcs <ejlarcombe@gmail.com>
2015-08-14 14:19:52 -0400

    crowdfunding info

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,22 @@
 -- your code here
+CREATE TABLE IF NOT EXISTS projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal TEXT,
+  start_date TEXT,
+  end_date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  project_id INTEGER,
+  user_id INTEGER
+);
